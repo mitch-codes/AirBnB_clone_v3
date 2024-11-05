0c8ad9fec64ac30a8896aa47ac48375415e6a596
mitch codes <mitchkodes@gmail.com>
2024-11-04 23:19:56 -0800

    add get function to get instance by id

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,6 +34,17 @@ class FileStorage:
             return new_dict
         return self.__objects
 
+    def get(self, cls, id):
+        """returns the dictionary __objects"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    if value.id == id:
+                        new_dict[key] = value
+            return new_dict
+        return self.__objects
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:

66db4d2c6a57508d624445a20f3440cba67624a8
mitch codes <mitchkodes@gmail.com>
2024-11-07 00:01:41 -0800

    add route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,21 @@
 
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 @app_views.route('/status')
 def status():
     """return status of our api"""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def stats():
+    """return stats for each column"""
+    fullstats = {}
+    for cls in classes:
+        fullstats[cls] = storage.count(classes[cls])
+        return jsonify(fullstats)

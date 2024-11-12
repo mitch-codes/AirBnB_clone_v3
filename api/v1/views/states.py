a7de14586ff4aa1fc8021e258a3f60b8af2dbd9c
mitch codes <mitchkodes@gmail.com>
2024-11-12 00:01:54 -0800

    return states api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/states', strict_slashes=False)
 def states():
     """return states objects as a list with all details"""
@@ -11,5 +12,15 @@ def states():
     response1 = storage.all()
     for key, value in response1.items():
         if key.split(".")[0] == "State":
+            states_list.append(value.to_dict())
+    return jsonify(states_list)
+
+@app_views.route('/states/<state_id>')
+def states_id(state_id):
+    """return states objects as a list with all details"""
+    states_list = []
+    response1 = storage.all()
+    for key, value in response1.items():
+        if key.split(".")[0] == "State" and value.id == state_id:
             states_list.append(value.to_dict())
     return jsonify(states_list)

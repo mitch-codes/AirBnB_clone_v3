0beea400df79eec1c1fd286b6ce562e020dc805f
mitch codes <mitchkodes@gmail.com>
2024-11-14 04:49:59 -0800

    add methods, all but PUT

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, make_response, abort, request
 from api.v1.views import app_views
 from models import storage
+from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False)
@@ -29,20 +30,20 @@ def states_id(state_id):
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def del_state(state_id):
-    """return states objects as a list with all details"""
+    """delete a state"""
     states_list = []
     response1 = storage.all()
     for key, value in response1.items():
         if key.split(".")[0] == "State" and value.id == state_id:
-            states_list.append(value.to_dict())
-            storage.delete(value)
-            storage.save()
+            states_list.append(value)
     if len(states_list) == 0:
         abort(404)
-    return make_response(jsonify({}), 200)
+    storage.delete(states_list[0])
+    storage.save()
+    return str(states_list[0])
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def add_state(state_id):
+def add_state():
     """create new state"""
     state_json = request.get_json()
     if state_json == None:
@@ -50,7 +51,7 @@ def add_state(state_id):
     if 'name' not in state_json:
         return make_response("Missing name", 400)
     new_state = State(**state_json)
-    storage.add(new_state)
+    storage.new(new_state)
     storage.save()
     return make_response(jsonify(state_json), 201)
 
@@ -59,7 +60,7 @@ def modify_state(state_id):
     """return state object after modification"""
     state_json = request.get_json()
     state_item = {}
-    for key, value in response1.items():
+    for key, value in state_json.items():
         if key.split(".")[0] == "State" and value.id == state_id:
             state_item = value.to_dict()
             for inkey, inval in state_json.items():
@@ -68,6 +69,6 @@ def modify_state(state_id):
             storage.delete(value)
             storage.save()
             new_state = State(**state_item)
-            storage.add(new_state)
+            storage.new(new_state)
             storage.save()
     return jsonify(state_item)
